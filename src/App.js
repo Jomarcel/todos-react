59ac4339fedc33466021336f0f535a53bfc93047
Jomarcel <jomarcel_n@outlook.com>
2020-04-08 03:33:07 +1000

    added materialise css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos  from './Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//root component to store the state of application
+class App extends Component {
+  state = { 
+    todos: [
+      {id: 1, content: 'anime'},
+      {id: 1, content: 'watch Kdrama'}
+    ]
+   }
+  render() { 
+    return (  
+      <div>
+        
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
